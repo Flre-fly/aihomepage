cd649cf388f2bf265830a181c701413dd4f53a10
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2022-03-27 12:07:08 +0900

    Initial commit

---
+++ src/main/java/demo/aihomepage/AihomepageApplication.java
@@ -0,0 +1,13 @@
+package demo.aihomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AihomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AihomepageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/aihomepage/AihomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.aihomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AihomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
